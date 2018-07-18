fda75e0c8168e5c251bdbfcaef9078a6451b87c8
Tasfik Rahman <tasfikrahman001@gmail.com>
2018-07-19 00:42:45 +0600

    [Subtraction]

---
+++ Subtraction.asm
@@ -0,0 +1,21 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+
+MAIN PROC
+    MOV BL,7
+    MOV BH,4
+    
+    SUB BL,BH   
+    ADD BL,48
+    
+    MOV AH,2
+    MOV DL,BL
+    INT 21H
+    
+    EXIT:   
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
